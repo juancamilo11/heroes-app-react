f97b7f547c2074430ce167728a042040f205cf25
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-11-07 16:07:49 -0500

    setup inicial del proyecto

---
+++ src/HeroesApp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeroesApp = () => {
+  return (
+    <div>
+      <h1>HeroesApp</h1>
+    </div>
+  );
+};
+
+export default HeroesApp;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import HeroesApp from "./HeroesApp";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<HeroesApp />, document.getElementById("root"));
