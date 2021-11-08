1c63cd440c4f6768e836fe6fabd6b3e5c1abcb18
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-11-08 00:13:06 -0500

    Demo proteccion de rutas terminado

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { AuthContext } from "../auth/AuthContext";
 import LoginScreen from "../pages/login/LoginScreen";
 import DashboardRoutes from "./DashboardRoutes";
+import PrivateRoute from "./PrivateRoute";
 
 const AppRouter = () => {
+  const { user } = useContext(AuthContext);
+
+  console.log(user);
+
   return (
     <Router>
       <div>
         <Switch>
           <Route exact path="/login" component={LoginScreen} />
-          <Route path="/" component={DashboardRoutes} />
+          <PrivateRoute
+            isAuthenticated={user.logged}
+            path="/"
+            component={DashboardRoutes}
+          />
         </Switch>
       </div>
     </Router>
