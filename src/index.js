4729b9fb00df36f3a04b0cf5dd01d36dca4d2701
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-11-08 01:42:40 -0500

    Demo loginScreen terminado

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import HeroesApp from "./HeroesApp";
+import "./index.css";
 
 ReactDOM.render(<HeroesApp />, document.getElementById("root"));
--- src/pages/login/LoginScreen.jsx
+++ src/pages/login/LoginScreen.jsx
@@ -1,27 +1,75 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../../auth/AuthContext";
 import types from "../../types/types";
+
+const formInitialState = {
+  username: "",
+  password: "",
+};
 
 const LoginScreen = ({ history }) => {
   const { dispatch } = useContext(AuthContext);
 
-  const handleLogin = () => {
-    // history.push("/");
+  const [formValues, setFormValues] = useState(formInitialState);
 
+  const validateInputs = () => {
+    if (formValues.username.length <= 2 || formValues.password.length <= 2) {
+      return false;
+    }
+    return true;
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (!validateInputs()) {
+      setFormValues(formInitialState);
+      window.alert("Username and password area required");
+      return;
+    }
     const lastPath = localStorage.getItem("lastPath") || "/";
-
-    dispatch({ type: types.login, payload: { name: "Camilo" } });
+    dispatch({ type: types.login, payload: { name: formValues.username } });
     history.replace(lastPath);
+  };
+
+  const handleInputChange = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    });
   };
 
   return (
     <div className="container">
-      <h1>Login</h1>
-      <hr />
+      <h2 className="text-center m-4">Heroes App - Login</h2>
+      <div className="form-container">
+        <div className="login-form">
+          <form onSubmit={handleLogin}>
+            <input
+              type="text"
+              name="username"
+              className="form-control mb-3 login-input"
+              placeholder="Username"
+              value={formValues.username}
+              onChange={handleInputChange}
+            />
 
-      <button className="btn btn-primary" onClick={handleLogin}>
-        Login
-      </button>
+            <input
+              type="password"
+              name="password"
+              className="form-control mb-3 login-input"
+              placeholder="Password"
+              value={formValues.password}
+              onChange={handleInputChange}
+            />
+            <button type="submit" className="btn btn-primary btn-ingresar">
+              Login
+            </button>
+            <p className="text-center mt-3 alert alert-danger">
+              Note: This is a demo Login
+            </p>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
